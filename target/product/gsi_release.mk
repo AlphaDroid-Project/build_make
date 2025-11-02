0b438351c4f9d70e6f9393dbdb1f8100fbf64014
elpaablo <donopwhile1@gmail.com>
2025-11-02 09:17:13 +0000

    Gimme some AlphaDroid

--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -28,7 +28,7 @@
 
 BUILDING_GSI := true
 ifneq ($(filter lineage_gsi_%,$(TARGET_PRODUCT)),)
-BUILDING_LINEAGE_GSI := true
+BUILDING_ALPHA_GSI := true
 endif
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
     SettingsIntelligence \
     frameworks-base-overlays
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(ALPHA_BUILD),)
 PRODUCT_PACKAGES += \
     LatinIME
 endif

206c4cfce6f2fc477f9ee799fecf32b85899ba89
elpaablo <donopwhile1@gmail.com>
2025-11-02 09:17:13 +0000

    Gimme some AlphaDroid

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -215,37 +215,27 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   # Build info
-  script.Print("                                         ");
-  script.Print("            _____            _     _     ");
-  script.Print("           |  __ \          (_)   | |    ");
-  script.Print("   ___ _ __| |  | |_ __ ___  _  __| |    ");
-  script.Print("  / __| '__| |  | | '__/ _ \| |/ _\`|    ");
-  script.Print(" | (__| |  | |__| | | | (_) | | (_| |    ");
-  script.Print("  \___|_|  |_____/|_|_ \___/|_|\__,_|  _ ");
-  script.Print("     /\             | |         (_)   | |");
-  script.Print("    /  \   _ __   __| |_ __ ___  _  __| |");
-  script.Print("   / /\ \ | '_ \ / _\`| '__/ _ \| |/ _\`|");
-  script.Print("  / ____ \| | | | (_| | | | (_) | | (_| |");
-  script.Print(" /_/    \_\_| |_|\__,_|_|  \___/|_|\__,_|");
-  script.Print("                                         ");
-
-  buildid = target_info.GetBuildProp("ro.crdroid.build.version")
+  version = target_info.GetBuildProp("ro.alpha.build.version")
+  variant = target_info.GetBuildProp("ro.alpha.build.variant")
   androidver = target_info.GetBuildProp("ro.build.version.release")
-  buildidn = target_info.GetBuildProp("ro.build.id")
-  buildday = target_info.GetBuildProp("ro.build.date")
-  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  secpatch = target_info.GetBuildProp("ro.build.version.security_patch")
+  builddate = target_info.GetBuildProp("ro.alpha.build.date")
   device = target_info.GetBuildProp("ro.product.name")
   manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
-  script.Print("***********************************************");
-  script.Print(" ROM version      : %s"%(buildid));
-  script.Print(" Android version  : %s"%(androidver));
-  script.Print(" Security patch   : %s"%(securep));
-  script.Print(" Build date       : %s"%(buildday));
-  script.Print("***********************************************");
-  script.Print(" Device           : %s"%(device));
-  script.Print(" Manufacturer     : %s"%(manufacturer));
-  script.Print("***********************************************");
-  script.Print("                                         ");
+  maintainer = target_info.GetBuildProp("ro.alpha.maintainer")
+  script.Print(" ");
+  script.Print("=== ALPHADROID PROJECT ===");
+  script.Print(" ");
+  script.Print(" Alpha version    : %s"%(version))
+  script.Print(" Build variant    : %s"%(variant))
+  script.Print(" Android version  : %s"%(androidver))
+  script.Print(" Security patch   : %s"%(secpatch))
+  script.Print(" Build date       : %s"%(builddate))
+  script.Print("")
+  script.Print(" Device           : %s"%(device))
+  script.Print(" Manufacturer     : %s"%(manufacturer))
+  script.Print(" Maintainer       : %s"%(maintainer))
+  script.Print("")
 
   device_specific.FullOTA_InstallBegin()
 
